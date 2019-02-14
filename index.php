bc18685a23a39084fef43390765223979c9b4bb0
YuriyDobryanskiy <dobryansky@i.ua>
2019-02-14 23:03:26 +0200

    First files

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <title>Hello, EPAM!</title>
+    
+    <!-- CSS -->    
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+
+  </head>
+  <body>
+    <div class="container-fluid main">
+    <h5>My old CV</h5>
+        <a href="files/Resume.pdf" target="_blank"><button type="button" class="btn btn-info">PDF</button></a>
+    </div>
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
