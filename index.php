9185b643cb63421d8527ff868610c9c3a47172d6
YuriyDobryanskiy <dobryansky@i.ua>
2019-02-18 04:19:31 +0200

    CRUD php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+if( isset($_POST['submit']) || isset($_GET['delete'])){
+  header("location: ".$_SERVER['PHP_SELF']);
+}
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -9,16 +14,61 @@
     <!-- CSS -->    
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-
+    <?php
+        require_once ('conn.php');
+        include ('func.php');
+    ?>    
   </head>
+
   <body>
     <div class="container-fluid main">
         <h5>My old CV</h5>
         <a><button type="button" class="btn btn-info">PDF</button></a>
     </div>
+    <hr/>
 
+    <?php
+      //select
+      selectList($link);      
+      //add      
+      if(isset($_POST['submit']) && ($_POST['id'] == '')){        
+        addStudent($link, $_POST['name'],$_POST['age'],$_POST['university']);
+      }
+      //delete
+      if(isset($_GET['delete'])){        
+        deleteStudent($link, $_GET['delete']);
+      }
+      //edit
+      if(isset($_GET['edit'])){        
+        $row = editStudent($link, $_GET['edit']);
+      }
+      //save edited
+      if(isset($_GET['edit']) && ($_POST['id'] != '')){        
+        saveEditStudent($link, $_POST['id'], $_POST['name'],$_POST['age'],$_POST['university']);
+      }
 
+      mysqli_close($link);    
+    ?> 
 
+    <!--FORM-->
+    <div class="main row justify-content-center">
+      <form action="" method="POST">
+        <div class="form-group">
+          <input type="text" name="name" placeholder="Name" value="<?=$row['firstName']?>" class="form-control">
+        </div>
+        <div class="form-group">
+          <input type="number" name="age" placeholder="Age" value="<?=$row['age']?>" class="form-control">
+        </div>
+        <div class="form-group">
+          <input type="text" name="university" placeholder="University" value="<?=$row['university']?>" class="form-control">
+        </div>
+        <div class="form-group">
+          <input type="hidden" name="id" value="<?=$row['id']?>">
+          <button type="submit" name="submit" class="btn btn-success">Save</button>
+        </div>  
+      </form>
+    </div>
+    <!--FORM-->
 
 
     <!-- Optional JavaScript -->
---
+++ student.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Фев 18 2019 г., 06:14
+-- Версия сервера: 5.5.38-log
+-- Версия PHP: 5.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `students`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `student`
+--
+
+CREATE TABLE IF NOT EXISTS `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` tinytext NOT NULL,
+  `lastName` tinytext NOT NULL,
+  `age` tinyint(4) NOT NULL,
+  `university` mediumtext NOT NULL,
+  `home` mediumtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=cp1251 AUTO_INCREMENT=78 ;
+
+--
+-- Дамп данных таблицы `student`
+--
+
+INSERT INTO `student` (`id`, `firstName`, `lastName`, `age`, `university`, `home`) VALUES
+(1, 'Yuriy', 'Dobryanskiy', 28, 'Lviv Polytechnic', 'lviv'),
+(2, 'yuriy', '', 29, 'La Laguna', 'Spain'),
+(3, 'XXX', '', 30, 'Breslau', 'Poland'),
+(4, 'ффф', '', 43, 'Dijon', 'France'),
+(65, 'dxe111', '', 0, '', ''),
+(77, 'NAME2', '', 34, 'some', '');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
